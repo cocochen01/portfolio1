a4b78dc02014d5926b5efec2d4d49d53c14366bc
Coco <cocochen12301@gmail.com>
2025-02-10 13:18:45 -0800

    feat: Installed tailwind, react-router-dom, three fiber, three drei

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Home, About, Projects, Contact } from './pages';
+import Navbar from './components/Navbar'
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+    return (
+        <main className = "bg-slate-300/20">
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/portfolio1" element={<Home />} />
+                    <Route path="/portfolio1/about" element={<About />} />
+                    <Route path="/portfolio1/projects" element={<Projects />} />
+                    <Route path="/portfolio1/contact" element={<Contact />} />
+                </Routes>
+            </Router>
+        </main>
+    )
 }
 
 export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <header className="header">
+            <NavLink to="/portfolio1" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+                <p className="blue-gradient_text">AH</p>
+            </NavLink>
+            <nav className="flex text-lg gap-7 font-medium">
+                <NavLink to="/portfolio1/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+                    About
+                </NavLink>
+                <NavLink to="/portfolio1/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+                    Projects
+                </NavLink>
+                <NavLink to="/portfolio1/contact" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+                    Contact
+                </NavLink>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
